7faaf05751217f951b8f8982e1027b9d3b93fabf
akihito.nakano <sora.akatsuki@gmail.com>
2019-04-21 18:38:49 +0900

    Add BlockHeader

--- src/main/kotlin/com/github/ackintosh/utxo/Block.kt
+++ src/main/kotlin/com/github/ackintosh/utxo/Block.kt
@@ -1,6 +1,7 @@
 package com.github.ackintosh.utxo
 
 class Block(
+    private val header: BlockHeader,
     private val transactions: List<Transaction>
 ) {
     fun transactionCounter() = transactions.count()
--- src/test/kotlin/com/github/ackintosh/utxo/BlockTest.kt
+++ src/test/kotlin/com/github/ackintosh/utxo/BlockTest.kt
@@ -6,10 +6,13 @@ import org.junit.jupiter.api.Test
 class BlockTest {
     @Test
     fun transactionCounter() {
-        val block = Block(listOf(
-            Transaction(inputs = emptyList(), outputs = emptyList()),
-            Transaction(inputs = emptyList(), outputs = emptyList())
-        ))
+        val block = Block(
+            header = BlockHeader(PreviousBlockHash("xxx")),
+            transactions = listOf(
+                Transaction(inputs = emptyList(), outputs = emptyList()),
+                Transaction(inputs = emptyList(), outputs = emptyList())
+            )
+        )
 
         assertEquals(2, block.transactionCounter())
     }
