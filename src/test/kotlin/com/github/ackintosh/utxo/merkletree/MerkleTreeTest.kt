77ea18861e9a3ae53ff13c3c8e0fd83f62792d93
akihito.nakano <sora.akatsuki@gmail.com>
2019-04-24 23:47:01 +0900

    Structuring test cases

--- src/test/kotlin/com/github/ackintosh/utxo/merkletree/MerkleTreeTest.kt
+++ src/test/kotlin/com/github/ackintosh/utxo/merkletree/MerkleTreeTest.kt
@@ -2,18 +2,22 @@ package com.github.ackintosh.utxo.merkletree
 
 import com.github.ackintosh.utxo.transaction.Hash
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
 class MerkleTreeTest {
-    @Test
-    fun build() {
-        val rootNode = MerkleTree.build(listOf(
-            Hash("xxx"), Hash("yyy")
-        ))
+    @Nested
+    inner class `build` {
+        @Test
+        fun onlyTwoHashes() {
+            val rootNode = MerkleTree.build(listOf(
+                Hash("xxx"), Hash("yyy")
+            ))
 
-        assertEquals(
-            Hash("1073df16287683171f8e0cc7e265c7715e4ff73f503e5adffe258aa1f2dca5cf"),
-            rootNode.hash
-        )
+            assertEquals(
+                Hash("1073df16287683171f8e0cc7e265c7715e4ff73f503e5adffe258aa1f2dca5cf"),
+                rootNode.hash
+            )
+        }
     }
 }
