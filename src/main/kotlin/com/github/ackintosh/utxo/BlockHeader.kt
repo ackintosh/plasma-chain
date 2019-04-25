932d49b91c512a752868e800f3ed275513af2ceb
akihito.nakano <sora.akatsuki@gmail.com>
2019-04-26 08:38:24 +0900

    BlockHeader has a MerkleRoot

--- src/main/kotlin/com/github/ackintosh/utxo/BlockHeader.kt
+++ src/main/kotlin/com/github/ackintosh/utxo/BlockHeader.kt
@@ -1,7 +1,10 @@
 package com.github.ackintosh.utxo
 
+import com.github.ackintosh.utxo.merkletree.MerkleTree
+
 class BlockHeader(
-    private val previousBlockHash: PreviousBlockHash
+    private val previousBlockHash: PreviousBlockHash,
+    private val merkleRoot: MerkleTree.Node
 )
 
 data class PreviousBlockHash(val hash: String)
--- src/test/kotlin/com/github/ackintosh/utxo/BlockTest.kt
+++ src/test/kotlin/com/github/ackintosh/utxo/BlockTest.kt
@@ -1,5 +1,6 @@
 package com.github.ackintosh.utxo
 
+import com.github.ackintosh.utxo.merkletree.MerkleTree
 import com.github.ackintosh.utxo.transaction.Transaction
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
@@ -7,12 +8,17 @@ import org.junit.jupiter.api.Test
 class BlockTest {
     @Test
     fun transactionCounter() {
+        val transactions = listOf(
+            Transaction(inputs = emptyList(), outputs = emptyList()),
+            Transaction(inputs = emptyList(), outputs = emptyList())
+        )
+
         val block = Block(
-            header = BlockHeader(PreviousBlockHash("xxx")),
-            transactions = listOf(
-                Transaction(inputs = emptyList(), outputs = emptyList()),
-                Transaction(inputs = emptyList(), outputs = emptyList())
-            )
+            header = BlockHeader(
+                previousBlockHash = PreviousBlockHash("xxx"),
+                merkleRoot = MerkleTree.build(transactions.map { it.transactionHash() })
+            ),
+            transactions = transactions
         )
 
         assertEquals(2, block.transactionCounter())
