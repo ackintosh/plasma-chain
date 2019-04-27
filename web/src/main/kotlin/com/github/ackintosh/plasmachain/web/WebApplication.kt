067f0b8e66967d46c962595aae77e6e075d519b6
akihito.nakano <sora.akatsuki@gmail.com>
2019-04-27 20:23:13 +0900

    add rest controller

--- web/src/main/kotlin/com/github/ackintosh/plasmachain/web/WebApplication.kt
+++ web/src/main/kotlin/com/github/ackintosh/plasmachain/web/WebApplication.kt
@@ -2,9 +2,17 @@ package com.github.ackintosh.plasmachain.web
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class WebApplication
+
+@RestController
+class Index {
+    @GetMapping("/")
+    fun welcome() = "Welcome to Plasma Chain!"
+}
 
 fun main(args: Array<String>) {
     runApplication<WebApplication>(*args)
