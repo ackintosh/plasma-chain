81a5c22a9b524fee3378c2af6655c7cbc42c5dd7
akihito.nakano <sora.akatsuki@gmail.com>
2019-04-28 11:57:12 +0900

    Add gRPC service

--- web/src/main/kotlin/com/github/ackintosh/plasmachain/web/WebApplication.kt
+++ web/src/main/kotlin/com/github/ackintosh/plasmachain/web/WebApplication.kt
@@ -1,5 +1,10 @@
 package com.github.ackintosh.plasmachain.web
 
+import com.github.ackintosh.plasmachain.web.proto.PlasmaChainGrpc
+import com.github.ackintosh.plasmachain.web.proto.Response
+import com.github.ackintosh.plasmachain.web.proto.Transaction
+import io.grpc.stub.StreamObserver
+import org.lognet.springboot.grpc.GRpcService
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.web.bind.annotation.GetMapping
@@ -12,6 +17,13 @@ class WebApplication
 class Index {
     @GetMapping("/")
     fun welcome() = "Welcome to Plasma Chain!"
+}
+
+@GRpcService
+class PlasmaChainGRpcService : PlasmaChainGrpc.PlasmaChainImplBase() {
+    override fun submitTransaction(request: Transaction?, responseObserver: StreamObserver<Response>?) {
+        super.submitTransaction(request, responseObserver)
+    }
 }
 
 fun main(args: Array<String>) {
