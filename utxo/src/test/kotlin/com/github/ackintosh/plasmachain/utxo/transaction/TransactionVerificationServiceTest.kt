17c2f82c05256e66d038b8bfd28229a6ca4b691c
akihito.nakano <sora.akatsuki@gmail.com>
2019-05-02 18:01:13 +0900

    Tweak test case structures

--- utxo/src/test/kotlin/com/github/ackintosh/plasmachain/utxo/transaction/TransactionVerificationServiceTest.kt
+++ utxo/src/test/kotlin/com/github/ackintosh/plasmachain/utxo/transaction/TransactionVerificationServiceTest.kt
@@ -3,71 +3,81 @@ package com.github.ackintosh.plasmachain.utxo.transaction
 import com.github.ackintosh.plasmachain.utxo.Address
 import com.github.ackintosh.plasmachain.utxo.SignatureService
 import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import java.security.interfaces.ECPrivateKey
 import java.security.interfaces.ECPublicKey
 
 class TransactionVerificationServiceTest {
-    @Test
-    fun verifyTransactionScript() {
-        val keyPair = Address.generateKeyPair()
-        val address = Address.from(keyPair)
+    @Nested
+    inner class `verifyTransactionScript()` {
+        @Nested
+        inner class `if scripts are valid` {
+            @Test
+            fun `return success result`() {
+                val keyPair = Address.generateKeyPair()
+                val address = Address.from(keyPair)
 
-        val output = Output(
-            amount = 100,
-            address = address
-        )
+                val output = Output(
+                    amount = 100,
+                    address = address
+                )
 
-        val transactionHash = Hash("xxx")
-        val outputIndex = OutputIndex(10u)
-        val input = Input(
-            transactionHash = transactionHash,
-            outputIndex = outputIndex,
-            signature = SignatureService.create(
-                keyPair.private as ECPrivateKey,
-                transactionHash,
-                outputIndex
-            ),
-            publicKey = keyPair.public as ECPublicKey
-        )
+                val transactionHash = Hash("xxx")
+                val outputIndex = OutputIndex(10u)
+                val input = Input(
+                    transactionHash = transactionHash,
+                    outputIndex = outputIndex,
+                    signature = SignatureService.create(
+                        keyPair.private as ECPrivateKey,
+                        transactionHash,
+                        outputIndex
+                    ),
+                    publicKey = keyPair.public as ECPublicKey
+                )
 
-        val result = TransactionVerificationService.verifyTransactionScript(
-            input = input,
-            utxo = output
-        )
+                val result = TransactionVerificationService.verifyTransactionScript(
+                    input = input,
+                    utxo = output
+                )
 
-        Assertions.assertTrue(result is TransactionVerificationService.Result.Success)
-    }
+                Assertions.assertTrue(result is TransactionVerificationService.Result.Success)
+            }
+        }
 
-    @Test
-    fun verifyTransactionScriptReturnsFailureResult() {
-        val keyPair = Address.generateKeyPair()
-        val address = Address.from(keyPair)
+        @Nested
+        inner class `if signature is not match` {
+            @Test
+            fun `return failure result`() {
+                val keyPair = Address.generateKeyPair()
+                val address = Address.from(keyPair)
 
-        val output = Output(
-            amount = 100,
-            address = address
-        )
+                val output = Output(
+                    amount = 100,
+                    address = address
+                )
 
-        val transactionHash = Hash("xxx")
-        val outputIndex = OutputIndex(10u)
-        val otherKeyPair = Address.generateKeyPair()
-        val input = Input(
-            transactionHash = transactionHash,
-            outputIndex = outputIndex,
-            signature = SignatureService.create(
-                otherKeyPair.private as ECPrivateKey,
-                transactionHash,
-                outputIndex
-            ),
-            publicKey = otherKeyPair.public as ECPublicKey
-        )
+                val transactionHash = Hash("xxx")
+                val outputIndex = OutputIndex(10u)
+                val otherKeyPair = Address.generateKeyPair()
+                val input = Input(
+                    transactionHash = transactionHash,
+                    outputIndex = outputIndex,
+                    signature = SignatureService.create(
+                        otherKeyPair.private as ECPrivateKey,
+                        transactionHash,
+                        outputIndex
+                    ),
+                    publicKey = otherKeyPair.public as ECPublicKey
+                )
 
-        val result = TransactionVerificationService.verifyTransactionScript(
-            input = input,
-            utxo = output
-        )
+                val result = TransactionVerificationService.verifyTransactionScript(
+                    input = input,
+                    utxo = output
+                )
 
-        Assertions.assertTrue(result is TransactionVerificationService.Result.Failure)
+                Assertions.assertTrue(result is TransactionVerificationService.Result.Failure)
+            }
+        }
     }
 }
