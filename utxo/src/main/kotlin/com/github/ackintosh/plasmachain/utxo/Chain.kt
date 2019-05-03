c148af5952bae4e2aef984a029703218baa77cbd
akihito.nakano <sora.akatsuki@gmail.com>
2019-05-03 15:20:15 +0900

    Move Alice information to Node from Chain

--- utxo/src/main/kotlin/com/github/ackintosh/plasmachain/utxo/Chain.kt
+++ utxo/src/main/kotlin/com/github/ackintosh/plasmachain/utxo/Chain.kt
@@ -3,34 +3,21 @@ package com.github.ackintosh.plasmachain.utxo
 import com.github.ackintosh.plasmachain.utxo.block.Block
 import com.github.ackintosh.plasmachain.utxo.block.Hash
 import com.github.ackintosh.plasmachain.utxo.block.Header
-import com.github.ackintosh.plasmachain.utxo.extensions.toHexString
 import com.github.ackintosh.plasmachain.utxo.merkletree.MerkleTree
 import com.github.ackintosh.plasmachain.utxo.transaction.CoinbaseData
 import com.github.ackintosh.plasmachain.utxo.transaction.GenerationInput
 import com.github.ackintosh.plasmachain.utxo.transaction.Output
 import com.github.ackintosh.plasmachain.utxo.transaction.Transaction
 
-class Chain {
-    val data = listOf(GENESIS_BLOCK)
-
-    fun dumpAliceInformation() =
-        mapOf(
-            "address" to ALICE_ADDRESS.value,
-            "private_key(hex encoded)" to ALICE_KEY_PAIR.private.encoded.toHexString(),
-            "public_key(hex encoded)" to ALICE_KEY_PAIR.public.encoded.toHexString()
-        )
+class Chain(address: Address) {
+    val data = listOf(generateGenesisBlock(address))
 
     companion object {
-        private val ALICE_KEY_PAIR = Address.generateKeyPair()
-        private val ALICE_ADDRESS = Address.from(ALICE_KEY_PAIR)
-
-        private val GENESIS_BLOCK = generateGenesisBlock()
-
-        private fun generateGenesisBlock(): Block {
+        private fun generateGenesisBlock(address: Address): Block {
             val transactions = listOf(
                 Transaction(
                     inputs = listOf(GenerationInput(CoinbaseData("xxx"))),
-                    outputs = listOf(Output(100, ALICE_ADDRESS))
+                    outputs = listOf(Output(100, address))
                 )
             )
 
