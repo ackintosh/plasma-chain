0aa770d17da8fac24cd7f100b9690021926199d9
akihito.nakano <sora.akatsuki@gmail.com>
2019-05-06 20:36:29 +0900

    Switch tx hash format to Plasma form

--- utxo/src/test/kotlin/com/github/ackintosh/plasmachain/utxo/transaction/TransactionTest.kt
+++ utxo/src/test/kotlin/com/github/ackintosh/plasmachain/utxo/transaction/TransactionTest.kt
@@ -3,7 +3,7 @@ package com.github.ackintosh.plasmachain.utxo.transaction
 import com.github.ackintosh.plasmachain.utxo.Address
 import com.github.ackintosh.plasmachain.utxo.SignatureCreationService
 import com.github.ackintosh.plasmachain.utxo.extensions.toHexString
-import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import java.math.BigInteger
 import java.security.interfaces.ECPrivateKey
@@ -16,14 +16,13 @@ class TransactionTest {
     private val hashY = TransactionHash(ByteArray(32) { 2.toByte() }.toHexString())
 
     private val inputX = {
-        val transactionHash = hashX
         val outputIndex = OutputIndex(0u)
         Input(
-            transactionHash = transactionHash,
+            transactionHash = hashX,
             outputIndex = outputIndex,
             signature = SignatureCreationService.create(
                 keyPair.private as ECPrivateKey,
-                transactionHash,
+                hashX,
                 outputIndex
             ),
             publicKey = keyPair.public as ECPublicKey
@@ -31,14 +30,13 @@ class TransactionTest {
     }.invoke()
 
     private val inputY = {
-        val transactionHash = hashY
         val outputIndex = OutputIndex(1u)
         Input(
-            transactionHash = transactionHash,
+            transactionHash = hashY,
             outputIndex = outputIndex,
             signature = SignatureCreationService.create(
                 keyPair.private as ECPrivateKey,
-                transactionHash,
+                hashY,
                 outputIndex
             ),
             publicKey = keyPair.public as ECPublicKey
@@ -54,9 +52,8 @@ class TransactionTest {
             output2 = Output(BigInteger("200"), address)
         )
 
-        assertEquals(
-            TransactionHash("caa6ed633715c8fd497cbea71060a0c8708b2820a226dcefb0af3e6e729608b2"),
-            transaction.transactionHash()
+        assertTrue(
+            transaction.transactionHash().value.length == 64
         )
     }
 }
