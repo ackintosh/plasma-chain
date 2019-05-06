bb4b6f03c631fb7b72241e54d32676e9c1be5ccf
akihito.nakano <sora.akatsuki@gmail.com>
2019-05-07 00:16:44 +0900

    Add a annotation to suppress warnings about experimental functions

--- node/src/test/kotlin/com/github/ackintosh/plasmachain/node/NodeTest.kt
+++ node/src/test/kotlin/com/github/ackintosh/plasmachain/node/NodeTest.kt
@@ -14,6 +14,7 @@ import java.math.BigInteger
 import java.security.interfaces.ECPrivateKey
 import java.security.interfaces.ECPublicKey
 
+@kotlin.ExperimentalUnsignedTypes
 class NodeTest {
     @Test
     fun sendCoinsToBob() {
