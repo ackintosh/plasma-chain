48d60e74e86c2b94a1c7d9db8567c7a4a3608b1c
akihito.nakano <sora.akatsuki@gmail.com>
2019-05-09 00:25:10 +0900

    Add smart contract wrapper generated via web3j

---
+++ node/src/main/gen/com/github/ackintosh/plasmachain/node/web3j/RootChain.java
@@ -0,0 +1,238 @@
+package com.github.ackintosh.plasmachain.node.web3j;
+
+import io.reactivex.Flowable;
+import io.reactivex.functions.Function;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import org.web3j.abi.EventEncoder;
+import org.web3j.abi.TypeReference;
+import org.web3j.abi.datatypes.Address;
+import org.web3j.abi.datatypes.Event;
+import org.web3j.abi.datatypes.Type;
+import org.web3j.abi.datatypes.generated.Bytes32;
+import org.web3j.abi.datatypes.generated.Uint256;
+import org.web3j.crypto.Credentials;
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.core.DefaultBlockParameter;
+import org.web3j.protocol.core.RemoteCall;
+import org.web3j.protocol.core.methods.request.EthFilter;
+import org.web3j.protocol.core.methods.response.Log;
+import org.web3j.protocol.core.methods.response.TransactionReceipt;
+import org.web3j.tx.Contract;
+import org.web3j.tx.TransactionManager;
+import org.web3j.tx.gas.ContractGasProvider;
+
+/**
+ * <p>Auto generated code.
+ * <p><strong>Do not modify!</strong>
+ * <p>Please use the <a href="https://docs.web3j.io/command_line.html">web3j command line tools</a>,
+ * or the org.web3j.codegen.SolidityFunctionWrapperGenerator in the 
+ * <a href="https://github.com/web3j/web3j/tree/master/codegen">codegen module</a> to update.
+ *
+ * <p>Generated with web3j version 4.2.0.
+ */
+public class RootChain extends Contract {
+    private static final String BINARY = "0x6101ae56600035601c52740100000000000000000000000000000000000000006020526f7fffffffffffffffffffffffffffffff6040527fffffffffffffffffffffffffffffffff8000000000000000000000000000000060605274012a05f1fffffffffffffffffffffffffdabf41c006080527ffffffffffffffffffffffffed5fa0e000000000000000000000000000000000060a052631f045c7660005114156100b35734156100ac57600080fd5b6001600055005b63d0e30db0600051141561012c57600034116100ce57600080fd5b60008054600182540110156100e257600080fd5b60018154018155503461014052336101605261014051610180526000546101a0527f73a19dd210f1a7f902193214c0ee91dd35ee5b4d920cba8d519eca65a7b488ca6060610160a1005b63d9caa3d2600051141561017e576020600461014037341561014d57600080fd5b61014051610160527f38ced01204841ece6186d0d51d96712da8d7553e3ef7cef2394bc185506ec03d6020610160a1005b63ade116f060005114156101a457341561019757600080fd5b60005460005260206000f3005b60006000fd5b6100046101ae036100046000396100046101ae036000f3";
+
+    public static final String FUNC___INIT = "__init";
+
+    public static final String FUNC_DEPOSIT = "deposit";
+
+    public static final String FUNC_SUBMIT = "submit";
+
+    public static final String FUNC_CURRENTDEPOSITBLOCKNUMBER = "currentDepositBlockNumber";
+
+    public static final Event DEPOSITED_EVENT = new Event("Deposited", 
+            Arrays.<TypeReference<?>>asList(new TypeReference<Address>() {}, new TypeReference<Uint256>() {}, new TypeReference<Uint256>() {}));
+    ;
+
+    public static final Event BLOCKSUBMITTED_EVENT = new Event("BlockSubmitted", 
+            Arrays.<TypeReference<?>>asList(new TypeReference<Bytes32>() {}));
+    ;
+
+    protected static final HashMap<String, String> _addresses;
+
+    static {
+        _addresses = new HashMap<String, String>();
+        _addresses.put("1557314556403", "0x30753E4A8aad7F8597332E813735Def5dD395028");
+    }
+
+    @Deprecated
+    protected RootChain(String contractAddress, Web3j web3j, Credentials credentials, BigInteger gasPrice, BigInteger gasLimit) {
+        super(BINARY, contractAddress, web3j, credentials, gasPrice, gasLimit);
+    }
+
+    protected RootChain(String contractAddress, Web3j web3j, Credentials credentials, ContractGasProvider contractGasProvider) {
+        super(BINARY, contractAddress, web3j, credentials, contractGasProvider);
+    }
+
+    @Deprecated
+    protected RootChain(String contractAddress, Web3j web3j, TransactionManager transactionManager, BigInteger gasPrice, BigInteger gasLimit) {
+        super(BINARY, contractAddress, web3j, transactionManager, gasPrice, gasLimit);
+    }
+
+    protected RootChain(String contractAddress, Web3j web3j, TransactionManager transactionManager, ContractGasProvider contractGasProvider) {
+        super(BINARY, contractAddress, web3j, transactionManager, contractGasProvider);
+    }
+
+    public List<DepositedEventResponse> getDepositedEvents(TransactionReceipt transactionReceipt) {
+        List<Contract.EventValuesWithLog> valueList = extractEventParametersWithLog(DEPOSITED_EVENT, transactionReceipt);
+        ArrayList<DepositedEventResponse> responses = new ArrayList<DepositedEventResponse>(valueList.size());
+        for (Contract.EventValuesWithLog eventValues : valueList) {
+            DepositedEventResponse typedResponse = new DepositedEventResponse();
+            typedResponse.log = eventValues.getLog();
+            typedResponse._depositer = (String) eventValues.getNonIndexedValues().get(0).getValue();
+            typedResponse._amount = (BigInteger) eventValues.getNonIndexedValues().get(1).getValue();
+            typedResponse._depositBlockNumber = (BigInteger) eventValues.getNonIndexedValues().get(2).getValue();
+            responses.add(typedResponse);
+        }
+        return responses;
+    }
+
+    public Flowable<DepositedEventResponse> depositedEventFlowable(EthFilter filter) {
+        return web3j.ethLogFlowable(filter).map(new Function<Log, DepositedEventResponse>() {
+            @Override
+            public DepositedEventResponse apply(Log log) {
+                Contract.EventValuesWithLog eventValues = extractEventParametersWithLog(DEPOSITED_EVENT, log);
+                DepositedEventResponse typedResponse = new DepositedEventResponse();
+                typedResponse.log = log;
+                typedResponse._depositer = (String) eventValues.getNonIndexedValues().get(0).getValue();
+                typedResponse._amount = (BigInteger) eventValues.getNonIndexedValues().get(1).getValue();
+                typedResponse._depositBlockNumber = (BigInteger) eventValues.getNonIndexedValues().get(2).getValue();
+                return typedResponse;
+            }
+        });
+    }
+
+    public Flowable<DepositedEventResponse> depositedEventFlowable(DefaultBlockParameter startBlock, DefaultBlockParameter endBlock) {
+        EthFilter filter = new EthFilter(startBlock, endBlock, getContractAddress());
+        filter.addSingleTopic(EventEncoder.encode(DEPOSITED_EVENT));
+        return depositedEventFlowable(filter);
+    }
+
+    public List<BlockSubmittedEventResponse> getBlockSubmittedEvents(TransactionReceipt transactionReceipt) {
+        List<Contract.EventValuesWithLog> valueList = extractEventParametersWithLog(BLOCKSUBMITTED_EVENT, transactionReceipt);
+        ArrayList<BlockSubmittedEventResponse> responses = new ArrayList<BlockSubmittedEventResponse>(valueList.size());
+        for (Contract.EventValuesWithLog eventValues : valueList) {
+            BlockSubmittedEventResponse typedResponse = new BlockSubmittedEventResponse();
+            typedResponse.log = eventValues.getLog();
+            typedResponse._root = (byte[]) eventValues.getNonIndexedValues().get(0).getValue();
+            responses.add(typedResponse);
+        }
+        return responses;
+    }
+
+    public Flowable<BlockSubmittedEventResponse> blockSubmittedEventFlowable(EthFilter filter) {
+        return web3j.ethLogFlowable(filter).map(new Function<Log, BlockSubmittedEventResponse>() {
+            @Override
+            public BlockSubmittedEventResponse apply(Log log) {
+                Contract.EventValuesWithLog eventValues = extractEventParametersWithLog(BLOCKSUBMITTED_EVENT, log);
+                BlockSubmittedEventResponse typedResponse = new BlockSubmittedEventResponse();
+                typedResponse.log = log;
+                typedResponse._root = (byte[]) eventValues.getNonIndexedValues().get(0).getValue();
+                return typedResponse;
+            }
+        });
+    }
+
+    public Flowable<BlockSubmittedEventResponse> blockSubmittedEventFlowable(DefaultBlockParameter startBlock, DefaultBlockParameter endBlock) {
+        EthFilter filter = new EthFilter(startBlock, endBlock, getContractAddress());
+        filter.addSingleTopic(EventEncoder.encode(BLOCKSUBMITTED_EVENT));
+        return blockSubmittedEventFlowable(filter);
+    }
+
+    public RemoteCall<TransactionReceipt> __init() {
+        final org.web3j.abi.datatypes.Function function = new org.web3j.abi.datatypes.Function(
+                FUNC___INIT, 
+                Arrays.<Type>asList(), 
+                Collections.<TypeReference<?>>emptyList());
+        return executeRemoteCallTransaction(function);
+    }
+
+    public RemoteCall<TransactionReceipt> deposit(BigInteger weiValue) {
+        final org.web3j.abi.datatypes.Function function = new org.web3j.abi.datatypes.Function(
+                FUNC_DEPOSIT, 
+                Arrays.<Type>asList(), 
+                Collections.<TypeReference<?>>emptyList());
+        return executeRemoteCallTransaction(function, weiValue);
+    }
+
+    public RemoteCall<TransactionReceipt> submit(byte[] _root) {
+        final org.web3j.abi.datatypes.Function function = new org.web3j.abi.datatypes.Function(
+                FUNC_SUBMIT, 
+                Arrays.<Type>asList(new org.web3j.abi.datatypes.generated.Bytes32(_root)), 
+                Collections.<TypeReference<?>>emptyList());
+        return executeRemoteCallTransaction(function);
+    }
+
+    public RemoteCall<BigInteger> currentDepositBlockNumber() {
+        final org.web3j.abi.datatypes.Function function = new org.web3j.abi.datatypes.Function(FUNC_CURRENTDEPOSITBLOCKNUMBER, 
+                Arrays.<Type>asList(), 
+                Arrays.<TypeReference<?>>asList(new TypeReference<Uint256>() {}));
+        return executeRemoteCallSingleValueReturn(function, BigInteger.class);
+    }
+
+    @Deprecated
+    public static RootChain load(String contractAddress, Web3j web3j, Credentials credentials, BigInteger gasPrice, BigInteger gasLimit) {
+        return new RootChain(contractAddress, web3j, credentials, gasPrice, gasLimit);
+    }
+
+    @Deprecated
+    public static RootChain load(String contractAddress, Web3j web3j, TransactionManager transactionManager, BigInteger gasPrice, BigInteger gasLimit) {
+        return new RootChain(contractAddress, web3j, transactionManager, gasPrice, gasLimit);
+    }
+
+    public static RootChain load(String contractAddress, Web3j web3j, Credentials credentials, ContractGasProvider contractGasProvider) {
+        return new RootChain(contractAddress, web3j, credentials, contractGasProvider);
+    }
+
+    public static RootChain load(String contractAddress, Web3j web3j, TransactionManager transactionManager, ContractGasProvider contractGasProvider) {
+        return new RootChain(contractAddress, web3j, transactionManager, contractGasProvider);
+    }
+
+    public static RemoteCall<RootChain> deploy(Web3j web3j, Credentials credentials, ContractGasProvider contractGasProvider) {
+        return deployRemoteCall(RootChain.class, web3j, credentials, contractGasProvider, BINARY, "");
+    }
+
+    @Deprecated
+    public static RemoteCall<RootChain> deploy(Web3j web3j, Credentials credentials, BigInteger gasPrice, BigInteger gasLimit) {
+        return deployRemoteCall(RootChain.class, web3j, credentials, gasPrice, gasLimit, BINARY, "");
+    }
+
+    public static RemoteCall<RootChain> deploy(Web3j web3j, TransactionManager transactionManager, ContractGasProvider contractGasProvider) {
+        return deployRemoteCall(RootChain.class, web3j, transactionManager, contractGasProvider, BINARY, "");
+    }
+
+    @Deprecated
+    public static RemoteCall<RootChain> deploy(Web3j web3j, TransactionManager transactionManager, BigInteger gasPrice, BigInteger gasLimit) {
+        return deployRemoteCall(RootChain.class, web3j, transactionManager, gasPrice, gasLimit, BINARY, "");
+    }
+
+    protected String getStaticDeployedAddress(String networkId) {
+        return _addresses.get(networkId);
+    }
+
+    public static String getPreviouslyDeployedAddress(String networkId) {
+        return _addresses.get(networkId);
+    }
+
+    public static class DepositedEventResponse {
+        public Log log;
+
+        public String _depositer;
+
+        public BigInteger _amount;
+
+        public BigInteger _depositBlockNumber;
+    }
+
+    public static class BlockSubmittedEventResponse {
+        public Log log;
+
+        public byte[] _root;
+    }
+}
