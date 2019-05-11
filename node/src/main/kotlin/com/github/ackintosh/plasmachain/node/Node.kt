9b94fbdea7dc50943a89be8b65696d74c363f8e1
akihito.nakano <sora.akatsuki@gmail.com>
2019-05-11 22:20:10 +0900

    Update test address

--- node/src/main/kotlin/com/github/ackintosh/plasmachain/node/Node.kt
+++ node/src/main/kotlin/com/github/ackintosh/plasmachain/node/Node.kt
@@ -173,7 +173,7 @@ class Node : Runnable {
         RootChain.load(
             RootChain.getPreviouslyDeployedAddress(ROOT_CHAIN_CONTRACT_NETWORK_ID),
             web3(),
-            ClientTransactionManager(web3(), "0x627306090abaB3A6e1400e9345bC60c78a8BEf57"),
+            ClientTransactionManager(web3(), "0xf17f52151EbEF6C7334FAD080c5704D77216b732"),
             DefaultGasProvider()
         )
 
